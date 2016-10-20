9545ed08dfce43f54dc427791555c6b6e4f5f22f
Deryck <285205451@qq.com>
2016-10-21 00:42:09 +0800

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 process.env.VUE_ENV = 'server'
 
 const isProd = process.env.NODE_ENV === 'production'
@@ -7,5 +9,22 @@ const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 
 const express = require('express')
-const favicon = require('serve-favicon')
-const serialize = require('serialize-javascript')
+// const favicon = require('serve-favicon')
+// const serialize = require('serialize-javascript')
+
+
+
+const app = express()
+
+const html = (() => {
+	const template =  fs.readFileSync(resolve('./index.html'), 'utf-8')
+	const i = template.indexOf('{{ APP }}')
+	const style = isProd ? '<link rel="stylesheet" href="/dist/styles.css">' : '' 
+
+	return {
+		head : template.slice(0, i ).replace('{{ STYLE }}', style),
+		tail : template.slice(i + '{{ APP }}'.length)
+	}
+})()
+
+console.log('isProd:'+isProd)
