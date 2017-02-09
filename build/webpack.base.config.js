367594e14d2e76c1829d7b0ac0c438b48de4490f
xiedangang <285205451@qq.com>
2017-02-10 00:22:09 +0800

    update code

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,13 +1,37 @@
 const path = require('path')
-
+const vueConfig = require('./vue-loader.config')
 
 module.exports = {
-	entry : {
-		app : './src/client-entry.js'
+	devtool: '#source-map',
+	entry: {
+		app: './src/client-entry.js'
+			// ,vendor: []
+	},
+	resolve: {
+		alias: {
+			'public': path.resolve(__dirname, '../public')
+		}
 	},
 	output: {
 		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/dist/',
 		filename: '[name][chunkhash].js'
 
+	},
+	module: {
+		rules: [{
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: vueConfig
+		}
+		, {
+			test: /\.js$/,
+			loader: 'buble-loader',
+			exclude: /node_modules/,
+			options: {
+				objectAssign: 'Object.assign'
+			}
+		}
+		]
 	}
 }
