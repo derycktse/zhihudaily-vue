58e319549d2782cc79947a7c12a1f16d62186ff1
xiedangang <285205451@qq.com>
2017-02-09 22:43:44 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+	mode: 'history',
+	scrollBehavior: () => ({
+		y: 0
+	}),
+	routes: [{
+		path: '/',
+		redirect: '/top'
+	}]
+})
