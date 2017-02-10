f4247acf620232cb9bb087c7aee6e33cf3ba3d2d
xiedangang <285205451@qq.com>
2017-02-10 23:21:02 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
 	<div id="app">
+    <div class="header navbar-fixed-top"><div class="container-fixed-width clearfix"><div class="top-nav-link"><a href="javascript:;" data-offset="470" class=""><span>浏览内容</span></a><a href="javascript:;" data-offset="0" class="active"><span>App 下载</span></a></div><h1 class="logo"><a href="http://daily.zhihu.com/" title="知乎日报" class="link-logo">知乎日报</a></h1></div></div>
 	</div>
 </template>
 
+<script>
+console.log(12)
+</script>
