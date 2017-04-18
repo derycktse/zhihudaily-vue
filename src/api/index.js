9c0c0e6aada905510ce39f8cc06d484fe038f1de
xiedangang <285205451@qq.com>
2017-04-18 16:33:22 +0800

    modify detail page

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ const API_ROOT = 'http://119.29.68.183:8088/'
 
 export default {
 	lastest: API_ROOT + 'news',
-	newbyid : API_ROOT + 'newsById'
+	newsbyid : API_ROOT + 'newsById'
 }
--- src/components/NewsDetail.vue
+++ src/components/NewsDetail.vue
@@ -1,5 +1,39 @@
 <template>
 	<div class="newscontent">
-		
+		<div v-html="imgProxy(newsDetailData.body || '')"></div>
 	</div>
 </template>
+
+<script>
+import axios from 'axios'
+import API from '../api/index'
+import * as Util from '../common/util'
+
+const fetchNewsDetail = (newsID,callback) =>{
+	axios.get(API.newsbyid,{
+		params : {
+			id : newsID
+		}
+	}).then(callback)
+}
+export default {
+	data(){
+		 return {
+		 	newsDetailData : {}
+		 }
+	},
+	mounted(){
+		let self = this
+		axios.get(API.newsbyid, {
+			params : {
+				id : this.$route.params.id
+			}
+		}).then((res)=>{
+			self.newsDetailData = res.data
+		})
+	},
+	methods:{
+		imgProxy : Util.replaceImageUrl
+	}
+}
+</script>
