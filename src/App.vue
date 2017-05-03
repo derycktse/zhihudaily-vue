b92f6e79076a08265d0f8863897fbe6a7e768fa0
xiedangang <285205451@qq.com>
2017-05-03 21:31:43 +0800

    add max-width

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  margin: 0 auto;
+  max-width: 640px;
+  box-shadow: 0 0 2.5em #5d656c;
 }
 
 header a{
