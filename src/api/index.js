47450d6ae61cc99f83da5821436d7fc54f0b4b44
xiedangang <285205451@qq.com>
2017-05-31 18:56:26 +0800

    change api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
-let API_ROOT = '/'
+let API_ROOT = 'https://derycktse-zhihuapi.herokuapp.com/getdata?url=' + 'https://news-at.zhihu.com/api/'
 
 export default {
-	lastest: API_ROOT + 'lastest',
-	newsbyid : API_ROOT + 'news/',
-	newsByDate : API_ROOT + 'before/'
+	lastest: API_ROOT + '3/stories/latest',
+	newsbyid : API_ROOT + '4/news/',
+	newsByDate : API_ROOT + '4/news/before/'
 }
