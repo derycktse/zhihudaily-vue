e8c8a25f643b4417093148afa8cfcae95e734239
xiedangang <285205451@qq.com>
2017-05-31 19:12:53 +0800

    init gh-pages

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-let API_ROOT = 'https://derycktse-zhihuapi.herokuapp.com/getdata?url=' + 'https://news-at.zhihu.com/api/'
-
-export default {
-	lastest: API_ROOT + '3/stories/latest',
-	newsbyid : API_ROOT + '4/news/',
-	newsByDate : API_ROOT + '4/news/before/'
-}
