1fc2bc21d87767fb22f84683ad456faefb441ac8
xiedangang <285205451@qq.com>
2017-06-01 14:25:16 +0800

    add postcss config&& add pxtorem

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,15 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-pxtorem": {
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    }
   }
 }
